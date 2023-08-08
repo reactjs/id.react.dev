4e24afaee513f60d3e769088c1bdb1aeb5f66527
Resi Respati <resir014@gmail.com>
2023-08-08 15:51:47 +0700

    docs: translate social banner component

--- src/components/SocialBanner.tsx
+++ src/components/SocialBanner.tsx
@@ -7,9 +7,9 @@ import {useRef, useEffect} from 'react';
 import cn from 'classnames';
 import {ExternalLink} from './ExternalLink';
 
-const bannerText = 'Support Ukraine 🇺🇦';
+const bannerText = 'Dukung Ukraina 🇺🇦';
 const bannerLink = 'https://opensource.fb.com/support-ukraine';
-const bannerLinkText = 'Help Provide Humanitarian Aid to Ukraine';
+const bannerLinkText = 'Donasikan Bantuan Kemanusiaan ke Ukraina';
 
 export default function SocialBanner() {
   const ref = useRef<HTMLDivElement | null>(null);
