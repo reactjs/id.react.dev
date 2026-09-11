beaa63f819a8fcd899e3e8dc8a84c34334095000
Zain Fathoni <me@zainf.dev>
2026-09-11 07:52:22 +0700

    fix: use Indonesian DocsFooter labels after sync

--- src/components/DocsFooter.tsx
+++ src/components/DocsFooter.tsx
@@ -87,7 +87,7 @@ function FooterLink({
       />
       <div className="flex flex-col overflow-hidden">
         <span className="text-sm font-bold tracking-wide no-underline uppercase text-secondary dark:text-secondary-dark group-focus:text-link dark:group-focus:text-link-dark group-focus:text-opacity-100">
-          {type === 'Previous' ? 'Previous' : 'Next'}
+          {type}
         </span>
         <span className="text-lg break-words group-hover:underline">
           {title}
